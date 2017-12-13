b0e4adffb29ab741ca12928dd76349694975c613
Drawn Owl <eugene.kovalev@me.com>
2017-12-13 20:33:29 +0300

    prepare for register users

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load Config
-require_once 'config/Config.php';
+require_once 'config/config.php';
 
 // Autoload Core Libraries
 spl_autoload_register(function ($className)
--- app/config/config.php
+++ app/config/config.php
@@ -3,14 +3,16 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_NAME', 'pdopostrs');
+define('DB_NAME', 'shareposts');
 define('DB_PORT', '8889');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', 'http://localhost:8888/MVC');
+define('URLROOT', 'http://localhost:8888/mvc');
 
 // Site Name
 define('SITENAME', 'SharePosts');
+
+define('APPVERSION', '0.1.1');
