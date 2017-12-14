bb0143df27cea4f494d4034231db2f3887db356e
Drawn Owl <eugene.kovalev@me.com>
2017-12-14 14:42:18 +0300

    user registration

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -4,7 +4,7 @@ class Users extends Controller
 {
     public function __construct()
     {
-
+        $this->userModel = $this->model('User');
     }
 
     public function register()
@@ -36,6 +36,12 @@ class Users extends Controller
             if (empty($data['email']))
             {
                 $data['email_err'] = 'Please enter email';
+            } else {
+                // Check email
+                if ($this->userModel->findUserByEmail($data['email']))
+                {
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
             if (empty($data['password']))
             {
@@ -60,7 +66,16 @@ class Users extends Controller
             if (empty($data['email_err']) && empty($data['name_err'])
                 && empty($data['password_err']) && empty($data['confirm_password_err']))
             {
-                die('SUCCESS');
+                // Hash password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // Register User
+                if ($this->userModel->register($data))
+                {
+                    redirect('users/login');
+                } else {
+                    die('Something went wrong');
+                }
             } else {
                 // Load view with errors
                 $this->view('users/register', $data);
