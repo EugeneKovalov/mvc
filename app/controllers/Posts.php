948d21c7f52252e457f6159d0728e8b067f778ab
Drawn Owl <eugene.kovalev@me.com>
2017-12-26 09:45:04 +0300

    posts - complete

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -151,6 +151,15 @@ class Posts extends Controller
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
+            // Get existing post from model
+            $post = $this->postModel->getPostById($id);
+
+            //Check for owner
+            if ($post->user_id != $_SESSION['user_id'])
+            {
+                redirect('posts');
+            }
+
             if ($this->postModel->deletePost($id))
             {
                 flash('post_message', 'Post Removed');
